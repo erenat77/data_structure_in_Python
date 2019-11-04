ec6db373f973269fae67097abfd2ebee8dc44934
aaron atmaca <50340279+erenat77@users.noreply.github.com>
2019-11-04 15:30:14 -0800

    Create convertString.py
    intial checkin

---
+++ convertString.py
@@ -0,0 +1,21 @@
+
+from linear_data_str import Stack
+
+rStack = Stack()
+
+def toStr(n,base):
+    convertString = "0123456789ABCDEF"
+    while n > 0:
+        if n < base:
+            rStack.push(convertString[n])
+        else:
+            rStack.push(convertString[n % base])
+        n = n // base
+    res = ""
+    while not rStack.isEmpty():
+        res = res + str(rStack.pop())
+    return res
+    
+#-------------------------------------
+#-------------TEST--------------------
+print(toStr(1453,16))
